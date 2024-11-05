de6d7529cec075a35d310cee8688f6959f3d3278
Jaxon Ruiz <45025385+jaxonRuiz@users.noreply.github.com>
2024-11-04 17:38:42 -0800

    added working button

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 // todo
+const button = document.createElement("button");
+button.addEventListener("click", () => {
+  alert("I have been clicked");
+});
+button.textContent = "Click me";
+
+document.body.appendChild(button);
